57f19906a1e76bc354fa06acf1df72f253655ba7
thatninja1 <63271160+thatninja1@users.noreply.github.com>
2026-02-06 21:04:36 -0500

    Fix plugin entrypoint to extend Hytale JavaPlugin lifecycle

--- src/main/java/dev/hytalemodding/hytalefarming/HytaleFarmingPlugin.java
+++ src/main/java/dev/hytalemodding/hytalefarming/HytaleFarmingPlugin.java
@@ -1,5 +1,7 @@
 package dev.hytalemodding.hytalefarming;
 
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import dev.hytalemodding.hytalefarming.commands.TokenTopCommand;
 import dev.hytalemodding.hytalefarming.commands.TokensBalanceCommand;
 import dev.hytalemodding.hytalefarming.commands.TokensPayCommand;
@@ -7,28 +9,42 @@ import dev.hytalemodding.hytalefarming.config.EnchantsConfig;
 import dev.hytalemodding.hytalefarming.config.TokensConfig;
 import dev.hytalemodding.hytalefarming.service.TokenService;
 
+import javax.annotation.Nonnull;
 import java.nio.file.Path;
 
-/**
- * SDK-agnostic plugin core so the project compiles with available dependencies.
- * Integrate these services into the active Hytale API layer in a follow-up adapter.
- */
-public class HytaleFarmingPlugin {
-    private final TokensConfig tokensConfig;
-    private final EnchantsConfig enchantsConfig;
-    private final TokenService tokenService;
+public class HytaleFarmingPlugin extends JavaPlugin {
+    private TokensConfig tokensConfig;
+    private EnchantsConfig enchantsConfig;
+    private TokenService tokenService;
 
-    private final TokensBalanceCommand balanceCommand;
-    private final TokensPayCommand payCommand;
-    private final TokenTopCommand topCommand;
+    private TokensBalanceCommand balanceCommand;
+    private TokensPayCommand payCommand;
+    private TokenTopCommand topCommand;
 
-    public HytaleFarmingPlugin(Path dataDirectory) {
+    public HytaleFarmingPlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        Path dataDirectory = getDataDirectory();
         this.tokensConfig = TokensConfig.load(dataDirectory.resolve("tokens.json"));
         this.enchantsConfig = EnchantsConfig.load(dataDirectory.resolve("enchants.json"));
         this.tokenService = new TokenService(dataDirectory, tokensConfig, enchantsConfig);
+
         this.balanceCommand = new TokensBalanceCommand(tokenService, tokensConfig);
         this.payCommand = new TokensPayCommand(tokenService, tokensConfig);
         this.topCommand = new TokenTopCommand(tokenService, tokensConfig);
+    }
+
+    @Override
+    protected void start() {
+        getLogger().atInfo().log("HytaleFarming plugin started.");
+    }
+
+    @Override
+    protected void shutdown() {
+        getLogger().atInfo().log("HytaleFarming plugin shutdown.");
     }
 
     public TokensBalanceCommand balanceCommand() { return balanceCommand; }
